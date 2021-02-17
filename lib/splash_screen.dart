d82c32137188470975e1f5441149b6bd242bbe28
brandx-jakyoung <jakyoung@brandxcorp.com>
2021-02-17 20:49:39 +0900

    Revert "Revert "Feat: 화면 구성 수정""
    This reverts commit 98ad88d025e8203d6ea506a9600ae0674a8ad12e.

---
+++ lib/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 1);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Image.asset(
+          'images/google_flutter_logo.png',
+          width: 158.9,
+          height: 45.3,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
