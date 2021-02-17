98ad88d025e8203d6ea506a9600ae0674a8ad12e
brandx-jakyoung <jakyoung@brandxcorp.com>
2021-02-17 20:49:26 +0900

    Revert "Feat: 화면 구성 수정"
    This reverts commit add4ba32c8aafea4235413b3425f7ab89c4f18f8.

--- lib/provider/theme_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-class ThemeProvider extends ChangeNotifier {
-  ThemeMode _mode;
-  ThemeProvider(this._mode);
-
-  ThemeMode get theme => _mode;
-}
