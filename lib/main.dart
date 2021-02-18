8a1d2950cb32866d7bdfdfb2ed6ecee485b7e7a4
jakyoung <wkrud203@gmail.com>
2021-02-18 23:07:42 +0900

    test theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,59 @@
-import 'package:damda/index.dart';
-import 'package:damda/provider/theme_provider.dart';
-import 'package:damda/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Bloc/NightMode.dart';
+import 'Bloc/Weather.dart';
 import 'package:provider/provider.dart';
+import 'Bloc/WeatherAPi.dart';
+import 'Screens/MainPage.dart';
+import 'settingsScreen/settings.dart';
 
-void main() => runApp(Main());
+void main() => runApp(MyApp());
 
-class Main extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider<ThemeProvider>.value(value: ThemeProvider())
-    ], child: MainWidget());
+    return  MultiProvider(
+          providers:[ChangeNotifierProvider<WeatherBloc>.value(
+        value: WeatherBloc()),
+       ChangeNotifierProvider<WeatherAPi>.value(
+        value: WeatherAPi()),
+        ChangeNotifierProvider<NightMode>.value(
+        value: NightMode())],
+        child:MainWidget()
+    );
+    
+    
   }
 }
 
 class MainWidget extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    print('test main');
+     NightMode  usertheme = Provider.of<NightMode>(context);
     return MaterialApp(
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
-      home: new SplashScreen(),
-      routes: <String, WidgetBuilder>{
-        '/HomeScreen': (BuildContext context) => new Index(),
-      },
+          //  color:Colors.white,
+           
+            theme:usertheme.getTheme(),
+            home: Scaffold(appBar:AppBar(
+              //leading:Icon(FontAwesomeIcons),
+              
+              elevation:0,
+              title:Text("Weather Now",style:TextStyle(fontWeight:FontWeight.normal,fontSize:20)),
+              leading:Icon(FontAwesomeIcons.cloud),
+              
+             
+            ),
+            floatingActionButton:Builder(
+                          builder:(context)=>FloatingActionButton(
+                onPressed:(){
+                  Navigator.push(context,MaterialPageRoute(builder:(context)=>Settings()));
+                },
+                child:Center(child: Icon(Icons.settings)),
+              ),
+            ),
+            body: SingleChildScrollView(child: MainPage())),
     );
   }
 }
