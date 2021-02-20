c7f57f1623fbbf176f5e89a0f2ae2e36d5e7b43d
jakyoung <wkrud203@gmail.com>
2021-02-20 17:15:27 +0900

    feat: 다크모드 구조 변경

--- lib/index.dart
+++ lib/index.dart
@@ -1,49 +1,37 @@
-import 'package:damda/pages/home.dart';
-import 'package:damda/pages/test.dart';
-
 import 'package:flutter/material.dart';
+import 'package:theme_mode_handler/theme_mode_handler.dart';
+import 'package:theme_mode_handler/theme_picker_dialog.dart';
 
-class Index extends StatefulWidget {
-  @override
-  _IndexState createState() => _IndexState();
-}
-
-class _IndexState extends State<Index> {
-  int _selectedIndex = 0;
-  final List<Widget> _children = [HomePage(), TestPage()];
-  void _onTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class Index extends StatelessWidget {
+  const Index({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final themeMode = ThemeModeHandler.of(context).themeMode;
+
     return Scaffold(
-      appBar: AppBar(title: Text('Light/Dark Theme')),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.grey,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.white.withOpacity(.60),
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        currentIndex: _selectedIndex, //현재 선택된 Index
-        onTap: _onTap,
-        items: [
-          BottomNavigationBarItem(
-            label: 'Favorites',
-            icon: Icon(Icons.favorite),
-          ),
-          BottomNavigationBarItem(
-            label: 'test',
-            icon: Icon(Icons.favorite),
-          )
-        ],
+      appBar: AppBar(
+        title: const Text('ThemeModeHandler Example'),
       ),
       body: Center(
-        child: _children[_selectedIndex],
+        child: Container(
+          width: 400,
+          padding: const EdgeInsets.all(50),
+          child: Card(
+            child: ListTile(
+              onTap: () => _selectThemeMode(context),
+              title: Text(themeMode.toString()),
+              subtitle: const Text('Tap to select another'),
+              trailing: const Icon(Icons.settings),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void _selectThemeMode(BuildContext context) async {
+    final newThemeMode = await showThemePickerDialog(context: context);
+    print(newThemeMode);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,95 +4,49 @@
 // import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 
-// void main() => runApp(Main());
-
-// class Main extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(providers: [
-//       ChangeNotifierProvider<ThemeProvider>.value(value: ThemeProvider())
-//     ], child: MainWidget());
-//   }
-// }
-
-// class MainWidget extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     print('test main');
-//     return MaterialApp(
-//       theme: ThemeData(),
-//       darkTheme: ThemeData.dark(),
-//       home: new SplashScreen(),
-//       routes: <String, WidgetBuilder>{
-//         '/HomeScreen': (BuildContext context) => new Index(),
-//       },
-//     );
-//   }
-// }
-
+import 'package:damda/index.dart';
+import 'package:damda/provider/theme_provider.dart';
+import 'package:damda/splash_screen.dart';
 import 'package:damda/theme_mode_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:theme_mode_handler/theme_mode_handler.dart';
-import 'package:theme_mode_handler/theme_picker_dialog.dart';
 
 void main() => runApp(Main());
 
-class Main extends StatefulWidget {
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<ThemeProvider>.value(value: ThemeProvider())
+    ], child: _Main());
+  }
+}
+
+class _Main extends StatefulWidget {
   @override
   _ExampleAppState createState() => _ExampleAppState();
 }
 
-class _ExampleAppState extends State<Main> {
+class _ExampleAppState extends State<_Main> {
   @override
   Widget build(BuildContext context) {
     return ThemeModeHandler(
       manager: ExampleThemeModeManager(),
       builder: (ThemeMode themeMode) {
         return MaterialApp(
-          themeMode: themeMode,
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-          ),
-          theme: ThemeData(
-            brightness: Brightness.light,
-          ),
-          home: HomePage(),
-        );
+            themeMode: themeMode,
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+            ),
+            theme: ThemeData(
+              brightness: Brightness.light,
+            ),
+            home: new SplashScreen(),
+            routes: <String, WidgetBuilder>{
+              '/HomeScreen': (BuildContext context) => new Index(),
+            });
       },
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final themeMode = ThemeModeHandler.of(context).themeMode;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ThemeModeHandler Example'),
-      ),
-      body: Center(
-        child: Container(
-          width: 400,
-          padding: const EdgeInsets.all(50),
-          child: Card(
-            child: ListTile(
-              onTap: () => _selectThemeMode(context),
-              title: Text(themeMode.toString()),
-              subtitle: const Text('Tap to select another'),
-              trailing: const Icon(Icons.settings),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _selectThemeMode(BuildContext context) async {
-    final newThemeMode = await showThemePickerDialog(context: context);
-    print(newThemeMode);
   }
 }
